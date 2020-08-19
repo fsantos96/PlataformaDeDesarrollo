9ffae5333a1dc5420d82c1c0f73dbbe269da9906
Federico Santos <federicopaulo@hotmail.com>
2020-08-19 08:43:28 -0300

    se sube el ejercico del juego y los 10 numeros

---
+++ 20200818/Ejercicio 10 numeros/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, numero = 0;
+            string mensaje;
+            Console.WriteLine("Debe ingresar 10 numeros, el ingreso se detendra si la suma de los numero supera los 50");
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese su numero");
+                numero = int.Parse(Console.ReadLine());
+                suma += numero;
+                if (suma >= 50)
+                {
+                    break;
+                }
+            }
+                      
+            mensaje = suma > 50 ? "El numero supera los 50" : "El resultado de la suma es " + suma;
+
+            Console.WriteLine(mensaje);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20200818/Juego/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, numeroSistema;
+            float apuesta = 0;
+            var random = new Random();
+            string mensaje;
+            Console.WriteLine("Bienvenido al juego de apuestas" + Environment.NewLine);
+            Console.WriteLine("Debe ingresar un numero entero, el cual sera comparado a una apuesta del sistema");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Debe ingresar un numero entero");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Debe ingresar su apuesta");
+            apuesta = float.Parse(Console.ReadLine());
+            numeroSistema = random.Next(numero + 1);
+
+            if (numero > numeroSistema)
+            {
+                mensaje = "Has ganado la apuesta duplicaste lo invertido: " + apuesta * 2;
+            } else if(numero < numeroSistema)
+            {
+                mensaje = "Has perdido la apuesta pierdes lo invertido ( apuesta del sistema:" + numeroSistema + ")";
+            } else
+            {
+                mensaje = "La apuesta resulto en empate, recibes lo invetido: " + apuesta;
+            }
+
+            Console.WriteLine(mensaje);
+            Console.ReadLine();
+        }
+    }
+}
