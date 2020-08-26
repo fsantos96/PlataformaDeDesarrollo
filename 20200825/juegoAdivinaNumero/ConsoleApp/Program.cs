86e06485642ba7aed85a5e1c2ffa1b55ebb1bb0b
Federico Santos <federicopaulo@hotmail.com>
2020-08-25 22:50:05 -0300

    se sube el juego adivina numero

---
+++ 20200825/juegoAdivinaNumero/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSecreto = 7, respuesta;
+            string mensaje ="";
+            Console.WriteLine("Intenta adivinar el numero secreto \n");
+
+            do
+            {
+                respuesta = int.Parse(Console.ReadLine());
+                mensaje = respuesta == numeroSecreto ? "Has Ganado" : "El numero no es correcto. Vuelve a intentar";
+                Console.WriteLine(mensaje + "\n");
+            }
+            while (respuesta != numeroSecreto);
+        }
+    }
+}
