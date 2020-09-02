5594adca7e6240ff1ad8ea46e5062a480bad93ca
Federico Santos <federicopaulo@hotmail.com>
2020-09-01 21:35:07 -0300

    juego adivina numero con random

---
+++ 20200901/juegoAdivinaNumeroRandom/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSecreto, respuesta, maxIntentos = 3;
+            Random random = new Random();
+            numeroSecreto = random.Next(10);
+
+            string mensaje = "";
+            Console.WriteLine("Intenta adivinar el numero secreto \n");
+
+            for (int i = 0; i < maxIntentos; i++)
+            {
+                respuesta = int.Parse(Console.ReadLine());
+                mensaje = respuesta == numeroSecreto ? "Has Ganado" : "El numero no es correcto. Vuelve a intentar";
+                if(respuesta == numeroSecreto)
+                {
+                    break;
+                }
+
+
+                if (i == maxIntentos - 1)
+                {
+                    mensaje = "Has perdido el numero era: " + numeroSecreto;
+                    break;
+                } 
+                
+                Console.WriteLine(mensaje + "\n");
+            }
+
+            Console.WriteLine(mensaje + "\n");
+        }
+    }
+}
