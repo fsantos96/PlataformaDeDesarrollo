04d0012c2feabb7bfefc3fc33ee3ca3de0b662e5
Federico Santos <federicopaulo@hotmail.com>
2020-09-01 22:33:50 -0300

    ejercicio año bisiesto

---
+++ 20200901/ejercicioAnioBiciesto/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio = ingresarAnioNacimiento("Ingrese su año de nacimiento");
+            string mensaje = "el año ";
+            mensaje += esBisiesto(anio) ? "es " : "no es ";
+            mensaje += "bisiesto";
+            Console.WriteLine(mensaje);
+        }
+
+        static int ingresarAnioNacimiento(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            return int.Parse(Console.ReadLine());
+        }
+
+        static bool esBisiesto(int anio)
+        {
+            bool esBisiesto;
+            if( anio % 4 == 0)
+            {
+                if (anio % 100 == 0)
+                {
+                    if (anio % 400 == 0)
+                    {
+                        esBisiesto = true;
+                    } else
+                    {
+                        esBisiesto = false;
+                    }
+                } else
+                {
+                    esBisiesto = true;
+                }
+            } else
+            {
+                esBisiesto = false;
+            }
+
+            return esBisiesto;
+        }
+    }
+}
