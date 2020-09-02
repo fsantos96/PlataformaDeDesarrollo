5edded5baecf30b6816e85856e2b3f6097382218
Federico Santos <federicopaulo@hotmail.com>
2020-09-01 21:55:51 -0300

    juego adivinar numero con funcion

---
+++ 20200901/juegoAdivinaNumeroFunciones/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                numero = solicitarNumero("Ingrese el numero");
+                if (numero == secreto) break;
+            }
+            if (numero == secreto)
+            {
+                Console.WriteLine("Ganaste");
+            }
+            else
+            {
+                Console.WriteLine("Perdiste el numero era " + secreto);
+            }
+        }
+
+        static int solicitarNumero(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
