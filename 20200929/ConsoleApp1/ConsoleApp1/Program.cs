7b2dc27fcd243c8c201019b02956f0e6c5362add
Federico Santos <federicopaulo@hotmail.com>
2020-09-30 09:02:31 -0300

    se sube el ejercicio con listas

---
+++ 20200929/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int> { 2, 6, 5, 8, 9, 45, 98, 90 };
+            List<int> numerosPares = numeros.Where(n => n % 2 == 0).OrderByDescending(num => num).ToList();
+            foreach(var num in numerosPares)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
