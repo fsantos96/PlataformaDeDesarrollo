8f2b62023a1ed4e8dfd939f0223c640cb99c353d
Federico Santos <federicopaulo@hotmail.com>
2020-10-13 22:40:14 -0300

    Clase Blazor

---
+++ 20201013/BlazorApp1/BlazorApp1/Data/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Data
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Vencimiento { get; set; }
+        public int Estimacion { get; set; }
+        //public Recurso Responsable { get; set; }
+        public int ResponsableId { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ 20201013/BlazorApp1/BlazorApp1/Data/TareaService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp1.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TareaService
+    {
+        public Task<Tarea[]> getTareas()
+        {
+            Tarea[] listaTarea = new Tarea[10];
+            for(var i = 0; i < 10; i++)
+            {
+                listaTarea[i] = new Tarea {Id=i, Estado= false, Estimacion=2, Titulo="Titulo" + i, Vencimiento=DateTime.Now };
+            }
+
+            return Task.FromResult(listaTarea);
+        }
+    }
+}
